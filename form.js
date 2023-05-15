83b0de263c1dd256927cc724f84462acc949130b
Hector Valdez <hvaldez180@gmail.com>
2023-05-15 00:04:10 -0400

    saving what i have

--- form.js
+++ form.js
@@ -5,6 +5,27 @@ const email = document.querySelector(".email");
 const password = document.querySelector(".password");
 const submitBtn = document.querySelector(".submit-btn");
 
-if (name == null) {
+if (name == null) {// means login page is open
+
+} else { // means signup page is open
+    submitBtn.addEventListener("click", () => {
+        fetch("/signup-user", {
+            method: "post",
+            headers: new Headers({"Content-Type": "application/json"}),
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.name) {
+                alert("register successful");
+            } else {
+                alert(data);
+            }
+        })
+    })
 
 }
--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const path = require("path");// allows us to know our html and css file location
 const bodyParser = require("body-parser");// allows us to send and recieve data
 const knex = require("knex");// allows us to access our database
 
+const db = knex({
+    client: "pg",
+    connection: {
+        host: "127.0.0.1",
+        user: "postgres",
+        password: "Brittring19",
+        database: "loginform",
+    }
+})
+
 const app = express();
 
 let initalPath = path.join(__dirname)
@@ -21,6 +31,31 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
     res.sendFile(path.join(initalPath, "signup.html"));
 }); // creates signup url path
+
+
+// post to the database
+app.post("/signup-user", (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name.length || !email.length || !password.length) {
+        res.json("fill all the fields");
+    } else {
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json([0])
+        })
+        .catch(err => {
+            if(err.detail.includes("already exists")) {
+                res.json("email already exists")
+            }
+        })
+    }
+})
 
 app.listen(3000, (req, res) => {
     console.log("listening on port 3000...")
